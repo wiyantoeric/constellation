9745465088b050ad221f6d4c5ad9bbed78cd70d9
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-10-17 23:09:53 +0700

    feat: update data

--- lib/common/constellation_data.dart
+++ lib/common/constellation_data.dart
@@ -3,62 +3,110 @@ import 'package:constellation/model/constellation.dart';
 final constellationData = <Constellation>[
   Constellation(
     constellationName: 'Aries',
-    imgUrl: 'https://example.com/aries.png',
-    desc: 'Aries is the first astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/aries-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/aries.png',
+    desc:
+        'Aries individuals are known for their boldness and leadership qualities. They are often enthusiastic, confident, and competitive.',
+    startDate: DateTime(DateTime.now().year, DateTime.january, 21),
+    endDate: DateTime(DateTime.now().year, DateTime.february, 19),
   ),
   Constellation(
     constellationName: 'Taurus',
-    imgUrl: 'https://example.com/taurus.png',
-    desc: 'Taurus is the second astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/taurus-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/taurus.png',
+    desc:
+        'Taureans are known for their practicality and determination. They tend to be reliable, patient, and have a strong sense of loyalty.',
+    startDate: DateTime(DateTime.now().year, DateTime.february, 20),
+    endDate: DateTime(DateTime.now().year, DateTime.march, 20),
   ),
   Constellation(
     constellationName: 'Gemini',
-    imgUrl: 'https://example.com/gemini.png',
-    desc: 'Gemini is the third astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/gemini-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/gemini.png',
+    desc:
+        'Geminis are typically curious and adaptable. They are known for their communication skills and versatility.',
+    startDate: DateTime(DateTime.now().year, DateTime.may, 21),
+    endDate: DateTime(DateTime.now().year, DateTime.june, 20),
   ),
   Constellation(
     constellationName: 'Cancer',
-    imgUrl: 'https://example.com/cancer.png',
-    desc: 'Cancer is the fourth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/cancer-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/cancer.png',
+    desc:
+        'Cancer individuals are known for their emotional sensitivity and nurturing qualities. They are often caring, empathetic, and protective.',
+    startDate: DateTime(DateTime.now().year, DateTime.june, 21),
+    endDate: DateTime(DateTime.now().year, DateTime.july, 22),
   ),
   Constellation(
     constellationName: 'Leo',
-    imgUrl: 'https://example.com/leo.png',
-    desc: 'Leo is the fifth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/leo-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/leo.png',
+    desc:
+        'Leos are confident and charismatic. They tend to be natural leaders, enthusiastic, and love being in the spotlight.',
+    startDate: DateTime(DateTime.now().year, DateTime.july, 23),
+    endDate: DateTime(DateTime.now().year, DateTime.august, 22),
   ),
   Constellation(
     constellationName: 'Virgo',
-    imgUrl: 'https://example.com/virgo.png',
-    desc: 'Virgo is the sixth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/virgo-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/virgo.png',
+    desc:
+        'Virgos are practical and detail-oriented. They are often seen as perfectionists, analytical, and helpful.',
+    startDate: DateTime(DateTime.now().year, DateTime.august, 23),
+    endDate: DateTime(DateTime.now().year, DateTime.september, 22),
   ),
   Constellation(
     constellationName: 'Libra',
-    imgUrl: 'https://example.com/libra.png',
-    desc: 'Libra is the seventh astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/libra-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/libra.png',
+    desc:
+        'Libras are known for their love of balance and harmony. They are often diplomatic, charming, and social.',
+    startDate: DateTime(DateTime.now().year, DateTime.september, 23),
+    endDate: DateTime(DateTime.now().year, DateTime.october, 22),
   ),
   Constellation(
     constellationName: 'Scorpio',
-    imgUrl: 'https://example.com/scorpio.png',
-    desc: 'Scorpio is the eighth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/scorpio-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/scorpio.png',
+    desc:
+        'Scorpios are intense and passionate. They are often seen as determined, resourceful, and loyal.',
+    startDate: DateTime(DateTime.now().year, DateTime.october, 23),
+    endDate: DateTime(DateTime.now().year, DateTime.november, 21),
   ),
   Constellation(
     constellationName: 'Sagittarius',
-    imgUrl: 'https://example.com/sagittarius.png',
-    desc: 'Sagittarius is the ninth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/sagittarius-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/sagittarius.png',
+    desc:
+        'Sagittarians are adventurous and optimistic. They are known for their love of freedom, enthusiasm, and wanderlust.',
+    startDate: DateTime(DateTime.now().year, DateTime.november, 22),
+    endDate: DateTime(DateTime.now().year, DateTime.december, 21),
   ),
   Constellation(
     constellationName: 'Capricorn',
-    imgUrl: 'https://example.com/capricorn.png',
-    desc: 'Capricorn is the tenth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/capricorn-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/.png',
+    desc:
+        'Capricorns are disciplined and hardworking. They are often seen as responsible, ambitious, and practical.',
+    startDate: DateTime(DateTime.now().year, DateTime.december, 22),
+    endDate: DateTime(DateTime.now().year, DateTime.january, 19),
   ),
   Constellation(
     constellationName: 'Aquarius',
-    imgUrl: 'https://example.com/aquarius.png',
-    desc: 'Aquarius is the eleventh astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/aquarius-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/aquarius.png',
+    desc:
+        'Aquarians are independent and open-minded. They are known for their innovation, humanitarian spirit, and intellectual curiosity.',
+    startDate: DateTime(DateTime.now().year, DateTime.january, 20),
+    endDate: DateTime(DateTime.now().year, DateTime.february, 18),
   ),
   Constellation(
     constellationName: 'Pisces',
-    imgUrl: 'https://example.com/pisces.png',
-    desc: 'Pisces is the twelfth astrological sign in the zodiac...',
+    imgUrl: 'assets/constellation_background/pisces-constellation.jpg',
+    logoUrl: 'assets/constellation_logo/pisces.png',
+    desc:
+        'Pisces individuals are empathetic and artistic. They are often seen as dreamy, compassionate, and creative.',
+    startDate: DateTime(DateTime.now().year, DateTime.february, 19),
+    endDate: DateTime(DateTime.now().year, DateTime.march, 20),
   ),
 ];
--- lib/model/constellation.dart
+++ lib/model/constellation.dart
@@ -1,11 +1,17 @@
 class Constellation {
   String constellationName;
   String imgUrl;
+  String logoUrl;
   String desc;
+  DateTime startDate;
+  DateTime endDate;
 
   Constellation({
     required this.constellationName,
     required this.imgUrl,
+    required this.logoUrl,
     required this.desc,
+    required this.startDate,
+    required this.endDate,
   });
 }
