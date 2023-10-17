81eba4f7c05548820dd5cb461bd817d26f428d62
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-10-17 19:07:13 +0700

    feat: create constellation data

---
+++ lib/common/constellation_data.dart
@@ -0,0 +1,64 @@
+import 'package:constellation/model/constellation.dart';
+
+final constellationData = <Constellation>[
+  Constellation(
+    constellationName: 'Aries',
+    imgUrl: 'https://example.com/aries.png',
+    desc: 'Aries is the first astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Taurus',
+    imgUrl: 'https://example.com/taurus.png',
+    desc: 'Taurus is the second astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Gemini',
+    imgUrl: 'https://example.com/gemini.png',
+    desc: 'Gemini is the third astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Cancer',
+    imgUrl: 'https://example.com/cancer.png',
+    desc: 'Cancer is the fourth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Leo',
+    imgUrl: 'https://example.com/leo.png',
+    desc: 'Leo is the fifth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Virgo',
+    imgUrl: 'https://example.com/virgo.png',
+    desc: 'Virgo is the sixth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Libra',
+    imgUrl: 'https://example.com/libra.png',
+    desc: 'Libra is the seventh astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Scorpio',
+    imgUrl: 'https://example.com/scorpio.png',
+    desc: 'Scorpio is the eighth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Sagittarius',
+    imgUrl: 'https://example.com/sagittarius.png',
+    desc: 'Sagittarius is the ninth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Capricorn',
+    imgUrl: 'https://example.com/capricorn.png',
+    desc: 'Capricorn is the tenth astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Aquarius',
+    imgUrl: 'https://example.com/aquarius.png',
+    desc: 'Aquarius is the eleventh astrological sign in the zodiac...',
+  ),
+  Constellation(
+    constellationName: 'Pisces',
+    imgUrl: 'https://example.com/pisces.png',
+    desc: 'Pisces is the twelfth astrological sign in the zodiac...',
+  ),
+];
