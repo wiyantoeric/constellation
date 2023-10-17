473b00fe62c22292c858a30b5e74792b0aa8b9f2
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-10-17 16:15:01 +0700

    feat(model): add constellation

---
+++ lib/model/constellation.dart
@@ -0,0 +1,11 @@
+class Constellation {
+  String constellationName;
+  String imgUrl;
+  String desc;
+
+  Constellation({
+    required this.constellationName,
+    required this.imgUrl,
+    required this.desc,
+  });
+}
